a4353b46008eb66ba3ee14bd7f100d76fae9ceb0
Jacob Duijzer <jacob@duijzer.com>
2024-04-08 11:51:43 +0200

    Trying out env vars

--- src/SliceRushPizza.ReceivePayment/OrderReceivedTrigger.cs
+++ src/SliceRushPizza.ReceivePayment/OrderReceivedTrigger.cs
@@ -4,10 +4,10 @@ using Microsoft.Extensions.Logging;
 
 namespace SliceRushPizza.ReceivePayment;
 
-public static class OrderReceivedTrigger
+public class OrderReceivedTrigger
 {
     [FunctionName("OrderReceivedTrigger")]
-    public static async Task RunAsync([ServiceBusTrigger("sbt-slicerushpizza-orders", "sub-slicerushpizza-orders", Connection = "AzureServiceBusOrdersListenConnectionString")] string mySbMsg,
+    public async Task RunAsync([ServiceBusTrigger("%NewOrdersTopicName%", "NewOrdersSubscriptionName", Connection = "AzureServiceBusOrdersListenConnectionString")] string mySbMsg,
         ILogger log)
     {
         log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
